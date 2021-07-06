c415d423a8c905cad4d527020f1b137a4ec178d2
Tbonexas <theclash090@gmail.com>
2021-07-06 04:16:49 -0700

    added photos for navlogo in assets folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
 
+
 function Navbar() {
     return (
         <>
