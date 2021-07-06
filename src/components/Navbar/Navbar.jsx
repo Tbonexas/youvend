35dcf317e39358e37bfe441dc6e4801b68582971
Tbonexas <theclash090@gmail.com>
2021-07-06 04:32:18 -0700

    added logo instead of html import

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
 
+import logo from '../../../assets/navbarlogo/navlogo.png'
+
 
 function Navbar() {
     return (
@@ -9,7 +11,7 @@ function Navbar() {
             <AppBar position='fixed' className={classes.AppBar} color='inherit'>
                 <Toolbar>
                     <Typography variant='h6' className={classes.title}>
-                        <img src={} alt='minilogo' height='25px' className={classes.image} />
+                        <img src={logo} alt='minilogo' height='25px' className={classes.image} />
                         50 Amp Cord Reel
                     </Typography>
                 </Toolbar>
