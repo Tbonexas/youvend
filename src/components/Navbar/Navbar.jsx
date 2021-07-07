95b8fa3bdc562b99cc86f8ca77633cb8c35e74d5
Tbonexas <theclash090@gmail.com>
2021-07-07 02:21:00 -0700

    fixed dependencies and navabr error with toolbar tag. navbar is even now.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
-
+import logo from '../../assets/navbarlogo/navlogo.png'
 
 
 const Navbar= () => {
     const classes = useStyles();
     return (
         <>
-            <AppBar position='fixed' className={classes.AppBar} color='inherit'>
+            <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title}>
-                        <img src="https://github.com/Tbonexas/50ampcordreel/blob/main/assets/navbarlogo/navlogo.png?raw=true" alt='minilogo' height='25px' className={classes.image} />
-                        50 Amp Cord Reel
+                    <Typography variant='h6' className={classes.title} color='inherit'>
+                        <img src={logo} alt='minilogo' height='25px' className={classes.image} />50 Amp Cord Reel
                     </Typography>
-                </Toolbar>
+                
                 <div className={classes.grow}/>
                 <div className={classes.button}>
                     <IconButton aria-label='Show cart items' color='inherit' >
@@ -24,6 +23,7 @@ const Navbar= () => {
                         </Badge>
                     </IconButton>
                 </div>
+                </Toolbar>
             </AppBar>
         </>
     )
