48deb28c947df7edf9330cb1bc94a2feab8b8132
Tbonexas <theclash090@gmail.com>
2021-07-07 03:02:18 -0700

    finalized navbar, added style2 to products page so the navbar is padded from products

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -56,9 +56,7 @@ export default makeStyles((theme) => ({
     color: 'inherit',
   },
   inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    padding: theme.spacing(1, 1, 1, 0),    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import Product from './Product';
+import useStyles from './Product/styles2'
+import { classes } from 'istanbul-lib-coverage';
 
 const products = [
-    {id: 1, name: "50A Cord Reel", description: '50 Amp Cord Reel', price: '$3,500' },
-    {id: 2, name: "Cable Storage Barrel", description: '50 Amp Cord Barrel', price: '$3,500' },
-    {id: 3, name: "Unit Dolly", description: 'Dolly to move the product', price: '$3,500' },
+    {id: 1, name: "50A Cord Reel", 
+    description: 'A cord winding unit designed to save time. It has a motorized self retracting and delivering feature of up to 150 feet. The unit is designed to setup temporary power easier than using your hands to wind or unwind the cables. ', price: '$3,500' },
 ];
 
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
        <Grid container justify="center" spacing={4}>
            {products.map((product)=>(
                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
