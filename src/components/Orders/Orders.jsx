9e4c1eed697e156ada1eb9a01993f42fa1c83a57
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-10-18 16:48:27 -0700

    Worked on styling on Orders page, among slight modifications on Login | Registration pages

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap'
+import './style.css'
 
 function Orders() {
 
   return (
     <Container fluid className="orders-section">
-      <Row style={{ justifyContent: "center", padding: "10px" }}>
+      <Row className='orders-section'>
         <Col
         md={7}
         style={{
-          justifyContent: 'center',
+          
           paddingTop: "30px"
         }} 
         >
-        <h1 style={{fontSize: '2.1em'}}>
+        <h1 style={{fontSize: '2.1em', color:'rgba(245, 181, 27)'}}>
           Orders
         </h1>
       </Col>
       </Row>
-      <Row style={{justifyContent: 'center'}}>
-        <Col>
+      <Row >
+        <Col style={{color:'white'}}>
         <h3>
         Track Your YouVend 3D Printing E-Commerce Iteam
         </h3>
