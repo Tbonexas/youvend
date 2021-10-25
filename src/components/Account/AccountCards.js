75754d39e864e0ed82f97fc6af47e0adb62f2dad
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-10-25 10:56:17 -0700

    Styled Account Page

---
+++ src/components/Account/AccountCards.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "./styles.css";
+
+function AccountCards(props) {
+  return (
+    <Card className="account-card-view">
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }}>
+          {props.description} 
+        </Card.Text>
+        <Button variant="primary" href={props.link} target="_blank">
+          <i className="cil-external-link">&nbsp;</i>
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+export default AccountCards;
