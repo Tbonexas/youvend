aa21eff7c3c321e5c41c9bddc1b17634135b16dd
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2021-11-05 10:28:18 -0700

    Return Policy Draft

--- src/components/ReturnPolicy/ReturnPolicy.jsx
+++ src/components/ReturnPolicy/ReturnPolicy.jsx
@@ -11,9 +11,42 @@ function ReturnPolicy() {
         </h1>
         <Row className='returnPolicy-description'>
           <Col style={{ color: 'white' }}>
-            <h3>
-              YouVend 3D Printing E-Commerce Shop Return Policy Stands as ......
-            </h3>
+            <div class="returnPolicy-text">
+              <p>YouVend accept returns and exchanges under these terms. You can return YouVend iteam within 30 days of your purchase with receipt or proof of purchase. If 30 days or more have passed since your purchase, we cannot offer you a refund or an exchange.
+
+                Upon receipt of the returned item, we will fully examine it and notify you via email, within a reasonable period of time, whether you are entitled to a return. If you are entitled to a return, we will refund your purchase price and a credit will automatically be applied to your original method of payment.
+
+                Only regular priced items may be refunded. Sale items are non-refundable.
+
+                To follow-up on the status of your return, please contact us at [•].</p>
+
+                <p><strong>Exchanges</strong></p>
+
+                <p>We only exchange goods if they are defective or damaged. In circumstances where you consider that a product is defective, you should promptly contact us at [•] with details of the product and the defect. You can send the item you consider defective to:</p>
+
+                 <p>[Address]</p>
+
+
+                <p>Upon receipt of the returned product, we will fully examine it and notify you via e-mail, within a reasonable period of time, whether you are entitled to a replacement as a result of the defect. If you are eligible, we will send you a replacement product.</p>
+
+
+                <p><strong>Exceptions</strong></p>
+
+                <p>Some items are non-refundable and non-exchangeable. These include: [list items]</p>
+
+
+                <p><strong>Shipping</strong></p> 
+
+                <p>To return the item you purchased, please mail it to:</p>
+
+
+
+                <p>[Address]</p>
+
+
+
+                <p>Refunds do not include any shipping and handling charges shown on the packaging slip or invoice. Shipping charges for all returns must be prepaid and insured by you. You are responsible for any loss or damage to hardware during shipment. We do not guarantee that we will receive your returned item. Shipping and handling charges are not refundable. Any amounts refunded will not include the cost of shipping.</p>
+            </div>
           </Col>
         </Row>
       </Container>
