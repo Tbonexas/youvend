d982ef81ed8b6606bbbfc1cec450e185b7583076
Slain Gretzky <theclash090@gmail.com>
2021-12-18 15:26:03 -0800

    branch swap

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,10 +1,4 @@
 import Commerce from '@chec/commerce.js';
 
 
-<<<<<<< HEAD
 export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
-=======
-// commerce key need to setup account with andrew and partners for income. // 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
-
->>>>>>> 8e05538c871f11dc7d369ad8ab39e475d57a7419
